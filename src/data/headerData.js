24fdf6ba58bbeb265d315fa2503b49fb412f1e45
haza233 <hazaajja@gmail.com>
2023-07-14 21:11:04 +0700

    first commit

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+
+export const headerData = {
+    name: 'Muhmmad Rifqi',
+    title: "Hello! I am",
+    desciption: "Hello! I am Muhammad Rifqi, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
+    image: 'https://i.ibb.co/Vj9PPX4/Whats-App-Image-2023-03-31-at-22-39-20.jpg',
+    // imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
+    resumePdf: 'https://docs.google.com/document/d/1WXjTS_qGur3i0bwN9vgC4bA52wq4trMr60e2XF4i7v0/edit?usp=sharing'
+}
